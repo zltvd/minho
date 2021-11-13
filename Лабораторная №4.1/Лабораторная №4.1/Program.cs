d6c4a35f4c8cc53f5dbddb7695fbba4a7b462087
zltvd <zolotova1303@mail.ru>
2021-11-13 21:34:44 +0700

    first

---
+++ Лабораторная №4.1/Лабораторная №4.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лабораторная__4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+            Console.WriteLine("by by!");
+        }
+    }
+}
