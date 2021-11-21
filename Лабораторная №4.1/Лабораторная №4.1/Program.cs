938189b9b30f9ffaeddc4baa6b464d9897fabe52
Дарья Золотова <zolotova1303@mail.ru>
2021-11-21 16:40:37 +0700

    else

--- Лабораторная №4.1/Лабораторная №4.1/Program.cs
+++ Лабораторная №4.1/Лабораторная №4.1/Program.cs
@@ -12,8 +12,33 @@ namespace Лабораторная__4._1
         {
             bool endApp = false;
             Console.WriteLine("your cute calculator");
-            Console.WriteLine("------------------------\n");
+            while (!endApp)
+            {
+                double result = 0;
+                string op = Console.ReadLine();
 
+                try
+                {
+                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("this operation will result in a mathematical error.\n");
+                    }
+                    else Console.WriteLine("your result: {0:0.##}\n", result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("oh no! an exception occurred trying to do the math.\n - details: " + e.Message);
+                }
+
+                Console.WriteLine("------------------------\n");
+
+                Console.Write("press 'n' and enter to close the app, or press any other key and Enter to continue: ");
+                if (Console.ReadLine() == "n") endApp = true;
+
+                Console.WriteLine("\n");
+            }
+            return;
         }
     }
 }
