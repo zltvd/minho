6dae2d4ff26b7a6f555eb49f8ccf6d4bc74429af
Дарья Золотова <zolotova1303@mail.ru>
2021-11-21 16:08:23 +0700

    typeB

--- Лабораторная №4.1/Лабораторная №4.1/Program.cs
+++ Лабораторная №4.1/Лабораторная №4.1/Program.cs
@@ -14,6 +14,20 @@ namespace Лабораторная__4._1
             Console.WriteLine("your cute calculator");
             Console.WriteLine("------------------------\n");
 
+            while (!endApp)
+            {
+                string numInput2 = "";
+
+                Console.Write("type another number, and then press Enter: ");
+                numInput2 = Console.ReadLine();
+
+                double cleanNum2 = 0;
+                while (!double.TryParse(numInput2, out cleanNum2))
+                {
+                    Console.Write("this is not valid input; please enter an integer value: ");
+                    numInput2 = Console.ReadLine();
+                }
+            }
         }
     }
 }
