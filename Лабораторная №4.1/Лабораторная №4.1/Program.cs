faecce80b8b75c4daaab83d9724db243fe5d962b
Дарья Золотова <zolotova1303@mail.ru>
2021-11-21 16:42:56 +0700

    add

--- Лабораторная №4.1/Лабораторная №4.1/Program.cs
+++ Лабораторная №4.1/Лабораторная №4.1/Program.cs
@@ -6,14 +6,28 @@ using System.Threading.Tasks;
 
 namespace Лабораторная__4._1
 {
+    class Calculator
+    {
+        public static double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN;
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             bool endApp = false;
             Console.WriteLine("your cute calculator");
-            Console.WriteLine("------------------------\n");
-
         }
     }
 }
